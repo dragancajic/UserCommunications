778a029bef008976fe35eb46a408fe1157e66ac7
Dragan Cajic <cajic_dragan@yahoo.com>
2021-09-27 14:57:49 +0200

    First Commit

---
+++ app/src/main/java/org/eu5/learn_pisio/usercommunications/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.eu5.learn_pisio.usercommunications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
